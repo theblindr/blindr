b8df673154e6cdbaee1423d3292660a8c91ed237
Louis Dion-Marcil <louis.dionmarcil@gmail.com>
2015-02-22 06:05:36 -0500

    Site web ébauche initiale

--- app/src/com/mchacks/blindr/models/FacebookProfileListener.java
+++
@@ -1,8 +0,0 @@
-package com.mchacks.blindr.models;
-
-import java.util.List;
-
-public interface FacebookProfileListener {
-
-	public void onProfilePicturesReceived(List<String> pictures);
-}
--- app/src/com/mchacks/blindr/models/IDestination.java
+++
@@ -1,6 +0,0 @@
-package com.mchacks.blindr.models;
-
-public interface IDestination {
-	
-	public String getId();
-}
